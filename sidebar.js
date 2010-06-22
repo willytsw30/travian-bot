60ade50035e7cc45deea8b2d0ce8ac322313e562
eyal.kibbar <eyal.kibbar@efc044ab-8b69-7e87-2007-82c0a5b53b24>
2010-06-22 20:18:19 +0000

    New feature:  - marketplace calculates resources on traders' arraival

---
+++ sidebar.js
@@ -0,0 +1,80 @@
+
+var SideBar = {};
+
+SideBar.addSendResources = function (tr) {
+	var td = document.createElement('td');
+	tr.appendChild(td);
+
+	var a = document.createElement('a');
+	var img = document.createElement('img');
+	img.src = 'img/x.gif';
+	img.className = 'r1';
+	img.alt = 'Send Resources';
+	img.title = 'Send Resources';
+	a.appendChild(img);
+
+	var coords = SideBar.gerRowCoords(tr);
+	a.href = 'build.php?z='+Map.xy2id(coords.x, coords.y)+'&gid=17';
+	td.appendChild(a);
+}
+
+SideBar.gerRowCoords = function (tr) {
+	var x = XPath.getInt('./td/div[@class="cox"]/text()', tr);
+	var y = XPath.getInt('./td/div[@class="coy"]/text()', tr);
+	return {x: x, y: y};
+}
+
+SideBar.addSendUnits = function (tr){
+	var td = document.createElement('td');
+	tr.appendChild(td);
+
+	var a = document.createElement('a');
+	var img = document.createElement('img');
+	img.src = 'img/x.gif';
+	img.className = 'unit u1';
+	img.alt = 'Send Units';
+	img.title = 'Send Units';
+	a.appendChild(img);
+	
+	var coords = SideBar.gerRowCoords(tr);
+	a.href = 'a2b.php?z='+Map.xy2id(coords.x, coords.y);
+	td.appendChild(a);
+}
+
+SideBar.decorateSelectedRow = function (tr) {
+	tr.onmouseover = function() {
+		XPath.getNode('.//td[@class="dot"]', this).className = 'dot hl';
+	}
+	tr.onmouseout = function() { 
+		XPath.getNode('.//td[@class="dot hl"]', this).className = 'dot';
+	}
+	
+}
+
+SideBar.removeCoords = function (tr) {
+	var td = XPath.getNode('./td[@class="aligned_coords"]', tr);
+	td.parentNode.removeChild(td);
+}
+
+SideBar.isRowCurrentVillage = function (tr) {
+	return XPath.getNode('.//td[@class="dot hl"]', tr) != null;
+}
+
+SideBar.applyAllChanges = function () {
+	var rows = XPath.getNodes('//*[@id="vlist"]//tbody/tr');
+	for (var i in rows) {
+		var tr = rows[i];
+		if (!SideBar.isRowCurrentVillage(tr)) {
+			SideBar.addSendResources(tr);
+			SideBar.addSendUnits(tr);
+			SideBar.decorateSelectedRow(tr);
+		}
+		
+		SideBar.removeCoords(tr);
+	}
+	
+}
+
+SideBar.applyAllChanges();
+
+
