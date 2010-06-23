982aa94c96e52012cf99885bf34974fa613d9c0c
eyal.kibbar <eyal.kibbar@efc044ab-8b69-7e87-2007-82c0a5b53b24>
2010-06-23 15:55:21 +0000

    New features:  - added marketplace send resources helper
    Bug fixes:
     - resource countdown now stops at 0

--- trunk/build.js
+++ trunk/build.js
@@ -40,33 +40,41 @@ Marketplace.addTraderResourceRow = function (traderTable, resources) {
 	tr.appendChild(td);
 	td.colspan = '2';
 	
-	var img = document.createElement('img');
-	img.className = 'r1';
-	img.src = "img/x.gif";
-	img.alt = "wood";
-	td.appendChild(img);
-	td.appendChild(document.createTextNode(parseInt(resources.wood)+" | "));
+	var capacity = Resources.getWherehouseCapacity();
+	var span;
 	
-	img = document.createElement('img');
-	img.className = 'r2';
-	img.src = "img/x.gif";
-	img.alt = "clay";
-	td.appendChild(img);
-	td.appendChild(document.createTextNode(parseInt(resources.clay)+" | "));
+	td.appendChild(Resources.createWoodIcon());
+	span = document.createElement('span');
+	span.appendChild(document.createTextNode(""+parseInt(resources.wood)));
+	if (resources.wood > capacity)
+		span.className = 'bad';
+	td.appendChild(span);
+	td.appendChild(document.createTextNode(" | "));
+		
+	td.appendChild(Resources.createClayIcon());
+	span = document.createElement('span');
+	span.appendChild(document.createTextNode(""+parseInt(resources.clay)));
+	if (resources.clay > capacity)
+		span.className = 'bad';
+	td.appendChild(span);
+	td.appendChild(document.createTextNode(" | "));
 	
-	img = document.createElement('img');
-	img.className = 'r3';
-	img.src = "img/x.gif";
-	img.alt = "iron";
-	td.appendChild(img);
-	td.appendChild(document.createTextNode(parseInt(resources.iron)+" | "));
+	td.appendChild(Resources.createIronIcon());
+	span = document.createElement('span');
+	span.appendChild(document.createTextNode(""+parseInt(resources.iron)));
+	if (resources.iron > capacity)
+		span.className = 'bad';
+	td.appendChild(span);
+	td.appendChild(document.createTextNode(" | "));
 	
-	img = document.createElement('img');
-	img.className = 'r4';
-	img.src = "img/x.gif";
-	img.alt = "crop";
-	td.appendChild(img);
-	td.appendChild(document.createTextNode(parseInt(resources.crop)));
+	capacity = Resources.getGranaryCapacity();
+	
+	td.appendChild(Resources.createCropIcon());
+	span = document.createElement('span');
+	span.appendChild(document.createTextNode(""+parseInt(resources.crop)));
+	if (resources.crop > capacity)
+		span.className = 'bad';
+	td.appendChild(span);
 	
 }
 
@@ -109,10 +117,126 @@ Marketplace.addTradersResourcePrediction = function () {
 	
 	}
 }
+Marketplace.getAvailabileTraders = function () {
+	var mer = XPath.getString('//*[@id="target_select"]//*[@class="mer"]/text()');
+	mer = mer.match(/[0-9]+\/[0-9]+/g).toString();
+	return parseInt(mer.split('/')[0]);
+}
+Marketplace.getTraderCapacity = function () {
+	return XPath.getInt('//*[@id="send_select"]//*[@class="max"][1]//a/text()');
+}
+
+Marketplace.getTotalCarring = function() {
+	wood = XPath.getNode('//*[@id="r1"]').value;
+	wood = (wood == '') ? 0 : parseInt(wood);
+	
+	clay = XPath.getNode('//*[@id="r2"]').value;
+	clay = (clay == '') ? 0 : parseInt(clay);
+	
+	iron = XPath.getNode('//*[@id="r3"]').value;
+	iron = (iron == '') ? 0 : parseInt(iron);
+	
+	crop = XPath.getNode('//*[@id="r4"]').value;
+	crop = (crop == '') ? 0 : parseInt(crop);
+	
+	return  wood + clay + iron + crop;
+}
+
+Marketplace.createTotalResourcesNode = function () {
+	var span = document.createElement('span');
+	span.calcSpaceLeft = function () {
+		var totalCapacity = Marketplace.getTraderCapacity() * Marketplace.getAvailabileTraders();
+		var totalCarry = Marketplace.getTotalCarring();
+		DOM.removeAllChildren(this);
+		this.appendChild(document.createTextNode(totalCarry+"/"+totalCapacity));
+		(totalCarry > totalCapacity) ? this.className = 'bad' : this.className = 'good';
+	}
+	span.calcSpaceLeft();
+	
+	// adding change listeners
+	var nodes = XPath.getNodes('//*[starts-with(@onmouseup, "add_res")]');
+	for (var i in nodes) {
+		nodes[i].onmouseup = (function(span, oldFunc) {
+			return function() { oldFunc(); span.calcSpaceLeft(); }
+		})(span, nodes[i].onmouseup);
+	}
+	nodes = XPath.getNodes('//*[starts-with(@onkeyup, "upd_res")]');
+	for (var i in nodes) {
+		nodes[i].onkeyup = (function(span, oldFunc) {
+			return function() { oldFunc(); span.calcSpaceLeft(); }
+		})(span, nodes[i].onkeyup);
+	}
+	
+	return span;
+}
+Marketplace.createAddAllResourcesLink = function () {
+	var a = document.createElement('a');
+	a.href = "#";
+	a.onclick = function () { return false; }
+	a.onmouseup = function() {
+		var rows = XPath.getNodes('//table[@id="send_select"]//tr[./td[1]/@class = "ico"]');
+		for (var i in rows) {
+			var cb = XPath.getNode('.//input[@type="checkbox"]', rows[i]);
+			if (cb == null || cb.checked)
+				XPath.getNode('.//*[starts-with(@onmouseup, "add_res")]', rows[i]).onmouseup();
+		}
+	}
+	a.appendChild(document.createTextNode('('+Marketplace.getTraderCapacity()+')'));
+	return a;
+}
+Marketplace.addResourcesCheckboxes = function () {
+	var cbs = new Array();
+	var rows = XPath.getNodes('//table[@id="send_select"]//tr[./td[1]/@class = "ico"]');
+	for (var i in rows) {
+		var cb = document.createElement('input');
+		cb.type = 'checkbox';
+		var rNum = parseInt(i)+1;
+		cb.defaultChecked = Settings.Marketplace['r'+rNum+'Checkbox'];
+		rows[i].appendChild(cb);
+		cbs.push(cb);
+	}
+	var cb = document.createElement('input');
+	cb.type = 'checkbox';
+	cb.onclick = (function (cbs) {
+		return function() {
+			for (var i in cbs)
+				cbs[i].checked = this.checked;
+		}})(cbs);
+	return cb;
+}
+
+
+Marketplace.addTradersCapacity = function () {
+	table = XPath.getNode('//table[@id="send_select"]');
+	tr = document.createElement('tr');
+	table.appendChild(tr);
+	
+	var td = document.createElement('td');
+	td.appendChild(document.createTextNode('Total'));
+	tr.appendChild(td);
+	
+	tr.appendChild(document.createElement('td'));
+	
+	td = document.createElement('td');
+	td.appendChild(Marketplace.createTotalResourcesNode());
+	tr.appendChild(td);
+	
+	td = document.createElement('td');
+	td.className = 'max';
+	td.appendChild(Marketplace.createAddAllResourcesLink());
+	tr.appendChild(td);
+	
+	td = document.createElement('td');
+	td.appendChild(Marketplace.addResourcesCheckboxes());
+	tr.appendChild(td);
+	
+	
+}
 
 Marketplace.applyAllChanges = function () {
 	if (Marketplace.isPageFit()) {
 		Marketplace.addTradersResourcePrediction();
+		Marketplace.addTradersCapacity();
 	}
 }
 
